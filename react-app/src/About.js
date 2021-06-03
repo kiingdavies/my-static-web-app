07977c21eaf8dec31a5d209fac745f2dd68de4e9
kiingdavies <daviesadedeji007@gmail.com>
2021-06-03 10:47:16 +0100

    About page edited

--- react-app/src/About.js
+++ react-app/src/About.js
@@ -5,14 +5,15 @@ const About = () => (
     <div className="content-title-group not-found">
       <h2 className="title">Product Wish List</h2>
       <p>
-        This project was created to help represent a fundamental app written
+        This project was created by Adedeji Davies to help represent a fundamental app written
         with React. The shopping theme is used throughout the app.
       </p>
       <br />
       <h2 className="title">Resources</h2>
+
       <ul>
         <li>
-          <a href="https://github.com/MicrosoftDocs/mslearn-staticwebapp">
+          <a href="https://github.com/kiingdavies/my-static-web-app">
             Code in GitHub
           </a>
         </li>
