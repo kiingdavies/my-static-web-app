c478c9214515c526abb5a1d20e340e27b74c165c
kiingdavies <daviesadedeji007@gmail.com>
2021-06-03 10:57:42 +0100

    Bold name

--- react-app/src/About.js
+++ react-app/src/About.js
@@ -5,7 +5,7 @@ const About = () => (
     <div className="content-title-group not-found">
       <h2 className="title">Product Wish List</h2>
       <p>
-        This project was created by Adedeji Davies to help represent a fundamental app written
+        This project was created by <b>Adedeji Davies</b> to help represent a fundamental app written
         with React. The shopping theme is used throughout the app.
       </p>
       <br />
